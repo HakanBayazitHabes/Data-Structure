b169fa4e80a5d4f1eaeb6c6befc553141f1cac3f
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-06-18 16:08:00 +0300

    Problem with "contains duplicate" solved

---
+++ ArraysAndLists/ContainsDuplicate/Program.cs
@@ -0,0 +1,39 @@
+﻿// Given an integer array 'nums', returns true if any value appears at least twice int he array, and return false if every elements is distinct 
+
+/*
+Example 1:
+
+Input : nums = [1,2,3,1]
+Output : true
+
+Example 2:
+
+Input : nums = [1,2,3,4]
+output: false
+*/
+
+
+int[] myArray = [3,3];
+
+bool Solution1()
+{
+    HashSet<int> mySet = [];
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (mySet.Contains(myArray[i])) return true;
+        mySet.Add(myArray[i]);
+    }
+    return false;
+}
+
+Console.WriteLine(Solution1());
+
+
+bool Solution2()
+{
+    return myArray.Length != new HashSet<int>(myArray).Count;
+}
+
+Console.WriteLine(Solution2());
+
--- BigONotationFunctions/obj/Debug/net8.0/BigONotationFunctions.AssemblyInfo.cs
+++ BigONotationFunctions/obj/Debug/net8.0/BigONotationFunctions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BigONotationFunctions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+112920d2b492fec7cb6d00361d2ea0cea0a1e153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BigONotationFunctions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BigONotationFunctions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
