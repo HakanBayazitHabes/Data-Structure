80a15e2c6bb897455706dc3631353236f1505b27
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-06-21 02:56:42 +0300

    Added : comment for majority problem

--- ArraysAndLists/MajorityElement/Program.cs
+++ ArraysAndLists/MajorityElement/Program.cs
@@ -1,9 +1,36 @@
 ﻿/*
+Majority Element
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+ 
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+
 Boyer Moore Algoritması
 
+Beklenilen BigO:
 
-
-Beklenilen 
 Birinci Örnek için 
     Time Complexity : O(n)
     Space Complexity : O(n)
@@ -14,7 +41,7 @@ Birinci Örnek için
 
 */
 
-int[] nums = [2, 3, 4, 1, 1, 1, 1, 5,6,6,6,6,6,6,6];
+int[] nums = [2, 3, 4, 1, 1, 1, 1, 5, 6, 6, 6, 6, 6, 6, 6];
 
 int MajorityElement()
 {
