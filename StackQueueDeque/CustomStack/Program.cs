85d7eeadfabb997a19cb4e2a8baff0bcf14b2c1b
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-06-22 15:23:29 +0300

    Added : CustomStack project to StackQueueDeque solution

---
+++ StackQueueDeque/CustomStack/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+Kendi Stack() imizi oluşturalım
+
+push(item)
+pop()
+showLast()
+size()
+isEmpty()
+
+*/
+
+namespace CustomStack
+{
+    public class CustomStack<T>
+    {
+        List<T> elements;
+        public CustomStack() => elements = [];
+
+        public bool IsEmpty() => elements.Count == 0;
+
+        public void Push(T element) => elements.Add(element);
+
+        public T Pop()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            T lastElement = elements.Last();
+            elements.RemoveAt(elements.Count - 1);
+            return lastElement;
+        }
+        public T ShowLast() => elements.LastOrDefault();
+
+        public int Size() => elements.Count;
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var myStack = new CustomStack<int>();
+
+            Console.WriteLine("Is Empty : " + myStack.IsEmpty());
+
+            myStack.Push(10);
+            myStack.Push(20);
+            myStack.Push(40);
+
+            Console.WriteLine("Show Last Element : " + myStack.ShowLast());
+
+            Console.WriteLine("Pop : " + myStack.Pop());
+            Console.WriteLine("Show Last Element : " + myStack.ShowLast());
+            Console.WriteLine("Pop : " + myStack.Pop());
+            Console.WriteLine("Show Last Element : " + myStack.ShowLast());
+            Console.WriteLine("Pop : " + myStack.Pop());
+            Console.WriteLine("Show Last Element : " + myStack.ShowLast());
+            Console.WriteLine("Pop : " + myStack.Pop());
+            Console.WriteLine("Show Last Element : " + myStack.ShowLast());
+
+        }
+    }
+}
