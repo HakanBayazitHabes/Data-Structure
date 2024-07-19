e5e550dbd5fcc76243a97bc9978b4b6c27e98b2e
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-07-19 19:37:41 +0300

    Fibonacci's solution : Recursion & Iteration

---
+++ Tree/FibonacciNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Solution
+{
+    public static int Fib(int n)
+    {
+        if (n == 1 || n == 0)
+            return n;
+
+        return Fib(n - 1) + Fib(n - 2);
+    }
+}
+
+public class Solution2
+{
+    public static int IterativeFibonacci(int n)
+    {
+        int x = 0, y = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            (x, y) = (y, x + y);
+        }
+        return x;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Solution.Fib(3));
+        Console.WriteLine(Solution2.IterativeFibonacci(3));
+    }
+}
+
+
+
+
+
