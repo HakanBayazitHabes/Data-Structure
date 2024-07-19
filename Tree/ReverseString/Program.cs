230f8ea1f76a147ded6e3c0f19fdc1aa247bfa53
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-07-19 18:26:41 +0300

    Added : ReverseString project
    The ReverseString project is added, which includes the Solution class with a method to reverse a given string.

---
+++ Tree/ReverseString/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+public class Solution
+{
+    public char[] ReverseString(char[] s)
+    {
+        int length = s.Length;
+
+        for (int i = 0; i < length / 2; i++)
+        {
+            // tupple
+            (s[length - 1 - i], s[i]) = (s[i], s[length - 1 - i]);
+            // char temp = s[i];
+            // s[i] = s[length - 1 - i];
+            // s[length - 1 - i] = temp;            
+        }
+        return s;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        char[] input = ['h', 'e', 'l', 'l', 'o'];
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.ReverseString(input));
+
+    }
+}
+
+
