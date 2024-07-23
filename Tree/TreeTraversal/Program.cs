b290ec11932d09e07292f8357dc146f66e635a0e
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-07-24 00:19:43 +0300

    Added : TreeTraversal project
    The TreeTraversal project is added, which includes the BinarySearchTree class with methods for inserting nodes, checking if a value exists, finding the minimum and maximum values, and performing breadth-first search traversal.

---
+++ Tree/TreeTraversal/Program.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+
+public class Node
+{
+    public int value;
+    public Node left = null;
+    public Node right = null;
+
+    public Node(int value)
+    {
+        this.value = value;
+    }
+}
+
+
+class BinarySearchTree
+{
+    public Node root = null;
+
+    public bool Insert(int value)
+    {
+        Node newNode = new(value);
+
+
+        if (root is null)
+        {
+            root = newNode;
+            return true;
+        }
+
+        Node tempNode = root;
+
+
+        while (true)
+        {
+            if (newNode.value == tempNode.value)
+                return false;
+
+            if (newNode.value < tempNode.value)
+            {
+                if (tempNode.left is null)
+                {
+                    tempNode.left = newNode;
+                    return true;
+                }
+                tempNode = tempNode.left;
+            }
+
+            else
+            {
+                if (tempNode.right is null)
+                {
+                    tempNode.right = newNode;
+                    return true;
+                }
+                tempNode = tempNode.right;
+
+            }
+        }
+    }
+
+    public bool Contains(int value)
+    {
+        Node tempNode = root;
+
+        while (tempNode != null)
+        {
+            if (value < tempNode.value)
+            {
+                tempNode = tempNode.left;
+            }
+            else if (value > tempNode.value)
+            {
+                tempNode = tempNode.right;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Node MinOfNode(Node currentNode)
+    {
+        while (currentNode.left != null)
+        {
+            currentNode = currentNode.left;
+        }
+
+        return currentNode;
+    }
+
+    public Node MaxOfNode(Node currentNode)
+    {
+        while (currentNode.right != null)
+        {
+            currentNode = currentNode.right;
+        }
+
+        return currentNode;
+    }
+
+    public List<int> BFS()
+    {
+        Node currentNode = root;
+        Queue myQueue = [];
+        List<int> values = [];
+        myQueue.Enqueue(currentNode);
+
+        while (myQueue.Count > 0)
+        {
+            currentNode = (Node)myQueue.Dequeue();
+            values.Add(currentNode.value);
+            if (currentNode.left is not null)
+            {
+                myQueue.Enqueue(currentNode.left);
+            }
+            if (currentNode.right is not null)
+            {
+                myQueue.Enqueue(currentNode.right);
+            }
+        }
+        return values;
+    }
+}
+
+
+class Program
+{
+    public static void Main()
+    {
+        BinarySearchTree binarySearchTree = new();
+
+        Console.WriteLine(binarySearchTree.Insert(38));
+        Console.WriteLine(binarySearchTree.Insert(19));
+        Console.WriteLine(binarySearchTree.Insert(69));
+        Console.WriteLine(binarySearchTree.Insert(12));
+        Console.WriteLine(binarySearchTree.Insert(24));
+        Console.WriteLine(binarySearchTree.Insert(59));
+        Console.WriteLine(binarySearchTree.Insert(95));
+
+        Console.WriteLine(string.Join(", ", binarySearchTree.BFS()));
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
